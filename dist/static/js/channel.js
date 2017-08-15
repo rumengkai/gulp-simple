ab31b735f7039d49041f6141cff43b094358ee3f
rumengkai <rumengkai@aliyun.com>
2017-08-15 12:11:47 +0800

    builg

--- dist/static/js/channel.js
+++ dist/static/js/channel.js
@@ -1,3 +1,15 @@
 $(function(){
   
 });
+
+
+function floatLayer() {
+  layer.open({
+  type: 1,
+  title: false,
+  closeBtn: 0,
+  shadeClose: true,
+  skin: 'yourclass',
+  content: '自定义HTML内容'
+});
+}
