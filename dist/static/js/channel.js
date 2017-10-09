25b5f00b0a7f2c19bf7bb5dbd3092cb8b4f60731
rumengkai <rumengkai@aliyun.com>
2017-10-09 12:08:26 +0800

    专栏详情页

--- dist/static/js/channel.js
+++ dist/static/js/channel.js
@@ -1,15 +1,3 @@
 $(function(){
-  
-});
 
-
-function floatLayer() {
-  layer.open({
-  type: 1,
-  title: false,
-  closeBtn: 0,
-  shadeClose: true,
-  skin: 'yourclass',
-  content: '自定义HTML内容'
 });
-}
