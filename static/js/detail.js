cc5cbb0f2f4a8f6010b6e2cbc927546e25593872
rumengkai <rumengkai@aliyun.com>
2017-10-10 15:17:20 +0800

    tip

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,2 +1,8 @@
 $(function(){
+  for(i=0;i<$("a").length;i++){
+  	if($("a")[i].innerText=="立 即 查 看"){
+  		$("a").parent("span").parent().parent().css('display','none');
+      $("#outlink").css('display','none');
+  	}
+  }
 });
